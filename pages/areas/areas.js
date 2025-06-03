0503902b51dacfaac14df5d59161d94aa38e8b4c
nicolasfsz <nicolas.desouza02062008@gmail.com>
2025-06-03 17:02:37 -0300

    nn aguento mais

--- pages/areas/areas.js
+++ pages/areas/areas.js
@@ -1,13 +1,33 @@
 document.addEventListener('DOMContentLoaded', async function() {
-    const area = "Mistieri"
-    const response = await fetch(`http://localhost:3000/brinquedos/areas${area}`, {
+    const urlParams = new URLSearchParams(window.location.search);
+    const area = urlParams.get('area');
+    const token = String(localStorage.getItem('token').replaceAll('"', ''));
+    const response = await fetch(`http://localhost:3000/brinquedos/area/${area}`, {
         method: 'GET',
         headers: {
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
+            'Authorization': "Bearer " + token
         },
-        authorization: "Bearer" + token
+     
     });
 
     const data = await response.json();
     console.log(data);
+    for (var brinquedo of data.resultado) {
+        console.log(brinquedo);
+
+        const grid = document.querySelector('.rides-grid');
+        grid.innerHTML = grid.innerHTML + `
+                <div class="rides-grid">
+            <div class="ride-card">
+                <div class="ride-image" style="background-image: url('https://blog.hopihari.com.br/wp-content/uploads/2022/04/ANDY1124-2-1024x683.jpg')"></div>
+                <div class="ride-info">
+                    <h3 class="ride-name">${brinquedo.name}</h3>
+                    <div class="ride-time">${brinquedo.waiting_time} min</div>
+                    <span class="ride-status status-busy">${brinquedo.status}</span>
+                </div>
+            </div>`
+    }
+
+    
 });
