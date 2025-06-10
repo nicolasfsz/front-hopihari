82101f48c0f6748a821027e95abef2f2062df7f2
nicolasfsz <nicolas.desouza02062008@gmail.com>
2025-06-10 11:19:10 -0300

    sfd

--- pages/areas/areas.js
+++ pages/areas/areas.js
@@ -20,7 +20,7 @@ document.addEventListener('DOMContentLoaded', async function() {
         grid.innerHTML = grid.innerHTML + `
                 <div class="rides-grid">
             <div class="ride-card">
-                <div class="ride-image" style="background-image: url('https://blog.hopihari.com.br/wp-content/uploads/2022/04/ANDY1124-2-1024x683.jpg')"></div>
+                <div class="ride-image" style="background-image: url('../../images/${brinquedo.image}')"></div>
                 <div class="ride-info">
                     <h3 class="ride-name">${brinquedo.name}</h3>
                     <div class="ride-time">${brinquedo.waiting_time} min</div>
